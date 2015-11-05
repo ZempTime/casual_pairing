df0ed66d0ce6af044be7e9617d17ab7b9543039a
Chris Zempel <zempel@efeqdev.com>
2015-11-04 22:20:52 -0600

    removes bin from linked dirs so i can console on server

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,7 +4,7 @@ set :repo_url, 'git@github.com:zemptime/casual_pairing.git'
 set :deploy_to, '/home/deploy/casual_pairing'
 
 set :linked_files, %w{config/database.yml config/secrets.yml}
-set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 namespace :deploy do
 
