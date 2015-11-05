eaeae0e9201138f2e4b93f18dd67d6e49598be60
Chris Zempel <zempel@efeqdev.com>
2015-11-04 22:10:29 -0600

    adds secrets.yml to be symlinked by cap

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,7 +3,7 @@ set :repo_url, 'git@github.com:zemptime/casual_pairing.git'
 
 set :deploy_to, '/home/deploy/casual_pairing'
 
-set :linked_files, %w{config/database.yml}
+set :linked_files, %w{config/database.yml config/secrets.yml}
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 namespace :deploy do
